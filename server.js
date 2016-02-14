0a494fecd72703da07fc377f2dea1db0ec358b0d
Alex Ladyzhensky <alex.ladyzhensky@gmail.com>
2016-02-14 09:07:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var http = require('http');
 
+
+
 var handleRequest = function(request, response) {
   response.writeHead(200);
   response.end("Hello World!");
 }
+
+// use mongodb client to initialize
+
+var MongoClient = require('mongodb').MongoClient;
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
 
 var www = http.createServer(handleRequest);
 www.listen(8080);
